3b2c7d0d79779fcb96f5adb283596fa8d56140ac
Antonio Contreras <drancope@gmail.com>
2022-07-03 20:31:02 +0200

    Leer y establecer conexiones a partir de archivo

--- Union-find.py
+++ Union-find.py
@@ -4,15 +4,35 @@ class UF:
         for i in range(N):
             self.UF.append(i)
     def union(self, p, q):
-        self.UF[q] = self.UF[p]
-#    def connected(p, q):
-#        return 2
+        if self.UF[p] == self.UF[q]:
+            return('Ya estaban conectados.')
+        min = 0;
+        if self.UF[p] < self.UF[q]:
+            min = p
+            max = q
+        else:
+            min = q
+            max = p
+        for i in self.UF:
+            if self.UF[i] == self.UF[max]:
+                self.UF[i] = self.UF[min]
+        return('Conexión realizada.')
+    def connected(self, p, q):
+        if self.UF[p] == self.UF[q]:
+            return True
+        else:
+            return False
 
-X = UF(8)
+import fileinput
+
+with fileinput.input(files= 'tinyUF.txt') as f:
+    for line in f:
+        if len(line)<4:
+            X = UF(int(line))
+            continue
+        p = int(line.split()[0])
+        q = int(line.split()[1])
+        if not(X.connected(p, q)):
+            X.union(p, q)
+
 print(X.UF)
-
-X.union(6, 3)
-
-print(X.UF)
-
-#comentario
